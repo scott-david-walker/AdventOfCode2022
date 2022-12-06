74a1678ecf50ab61f879642631c08ad0b21642c3
Scott Walker <scott.david.walker@hotmail.com>
2022-12-06 08:09:04 +0100

    feat: day 6

---
+++ DaySix/DaySix/Program.cs
@@ -0,0 +1,22 @@
+﻿// 4 for part one
+// 14 for part two
+const int amountToSkip = 14;
+var data = File.ReadAllText("Data/Input.txt");
+var characterLocation = 0;
+var initialData = data.Substring(0, amountToSkip).ToList();
+
+var charCounter = amountToSkip;
+foreach (var c in data.Skip(amountToSkip))
+{
+    initialData.RemoveAt(0);
+    initialData.Add(c);
+    charCounter++;
+    if (initialData.Distinct().Count() != amountToSkip)
+    {
+        continue;
+    }
+
+    break;
+}
+
+Console.WriteLine(charCounter);
