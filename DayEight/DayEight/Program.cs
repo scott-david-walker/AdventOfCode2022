a14ca7372fb5bf17d5b62d8667b6ee958bc57c6f
Scott Walker <scott.david.walker@hotmail.com>
2022-12-08 16:15:12 +0100

    feat: nowhere yet

--- DayEight/DayEight/Program.cs
+++ DayEight/DayEight/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var lines = File.ReadLines("Data/input.txt");
+
+var gridWidth = lines.FirstOrDefault().Length;
+var gridLength = lines.Count();
+var tress = new Tree[gridLength,gridWidth];
+
+var length = 0;
+foreach (var line in lines)
+{
+    var chars = line.ToCharArray();
+
+    for(var i = 0; i < chars.Length - 1; i++)
+    {
+        tress[length,i] = new Tree();
+    }
+    length++;
+}
+class Tree
+{
+    public bool IsBlockedFromAbove { get; set; }
+    public bool IsBlockedFromBelow { get; set; }
+    public bool IsBlockedFromLeft { get; set; }
+    public bool IsBlockedFromRight { get; set; }
+}
